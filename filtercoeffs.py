d90b6cc5153dcf3075bbf54cd0ac3a2a9799c10d
Sam McSweeney <sammy.mcsweeney@gmail.com>
2019-10-18 18:38:00 +0800

    Still working on this filter script

--- filtercoeffs.py
+++ filtercoeffs.py
@@ -1,4 +1,4 @@
-from scipy import kaiser
+from scipy import kaiser, hanning, hamming, bartlett, blackman
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -12,14 +12,19 @@ axs[0].plot(rnorm, label="True coefficients, normalised")
 ntaps = 12
 nsamps = 128
 N = ntaps*nsamps
-k = kaiser(N, 14)
-for a in [5, 6, 7, 8]:
-    axs[0].plot(kaiser(N,a), label="Kaiser {0}".format(a))
+a = 6
+k = kaiser(N, a)
+axs[0].plot(kaiser(N,a), label="Kaiser {0}".format(a))
+
+hn = hanning(N)
+hm = hamming(N)
+br = bartlett(N)
+bl = blackman(N)
 
 x = (np.linspace(0, N, N, endpoint=False) - N/2 + 1)/N
 s = np.sinc(x*12)
 r1 = s*k
-axs[0].plot(s, label="Kaiser * sinc")
+axs[0].plot(r1, label="Kaiser * sinc")
 axs[0].plot(rnorm/s, label="rnorm/sinc")
 
 axs[1].plot(r1 - rnorm, label="Residuals")
