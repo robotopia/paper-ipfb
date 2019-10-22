d908b9c9363028c9b67f0592b3f71fe61ae90770
Sam McSweeney <sammy.mcsweeney@gmail.com>
2019-10-22 09:50:59 +0800

    Improved the filter analysis plot

--- filtercoeffs.py
+++ filtercoeffs.py
@@ -2,7 +2,7 @@ from scipy import hanning
 import numpy as np
 import matplotlib.pyplot as plt
 
-fig, axs = plt.subplots(2)
+fig, axs = plt.subplots(2, figsize=(5,7))
 
 r = np.loadtxt("r.txt")
 rmax = max(r)
@@ -20,6 +20,8 @@ hn = hanning(M+1)[1:]
 r1 = s*hn*(rmax-0.125)
 rounded = np.round(r1)
 
+'''
+# Plots to verify the constructed coefficients vs the actual coefficients.
 axs[0].plot(r, label="True coefficients")
 axs[0].plot(r1, label="Hanning * sinc")
 
@@ -31,14 +33,30 @@ print("rounded residuals: min = {0};   max = {1}".format(min(r1-r), max(r1-r)))
 axs[0].legend()
 axs[1].legend()
 plt.show()
+'''
 
-plt.plot(m, hn, 'k:', label="Hanning window")
-plt.plot(m, s, 'k--', label="Sinc function")
-plt.plot(m, s*hn, 'k-', label="Analysis filter")
+# Plots to go in the figure
+axs[0].plot(m, hn, 'k:', label="Hanning window")
+axs[0].plot(m, s, 'k--', label="Sinc function")
+axs[0].plot(m, s*hn, 'k-', label="Analysis filter")
 
-plt.xlabel("$m$")
-plt.ylabel("$h[m]$")
-plt.xlim([0,1536])
+axs[0].set_xlabel("$m$")
+axs[0].set_ylabel("$h[m]$")
+axs[0].set_xlim([0,1536])
 
-plt.legend()
+ctr = M//2 - 1
+k = (np.arange(M) - ctr)/P * 10 # i.e. 10 kHz
+fftr = np.roll(np.abs(np.fft.fft(s*hn)), ctr)
+dB = 10*np.log10(fftr/max(fftr))
+axs[1].plot(k, dB, 'k-')
+
+axs[1].set_xlabel("$\\nu$ (kHz)")
+axs[1].set_ylabel("Normalised power (dB)")
+axs[1].set_xlim([-30,30])
+axs[1].set_ylim([-60, 5])
+plt.grid(True)
+
+axs[0].legend(loc='upper right')
+#plt.show()
+plt.tight_layout()
 plt.savefig("filter.eps")
